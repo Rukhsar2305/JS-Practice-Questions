071804622d8cf59e7a0b00d0ca7f0edd017f0c5e
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-05 20:54:41 +0530

    Add files via upload

---
+++ Day-1/script.js
@@ -0,0 +1,33 @@
+// What is the value of age after this code runs
+
+let age = 23;
+age + 2; //after 2 years
+//ans: 23
+
+//What is the value of avg after this code runs
+
+let hindi = 80;
+let eng = 90;
+let maths = 100;
+let avg = (hindi+eng+maths) /3;
+//ans: 90
+
+//What is the value of each variable in each line of code?
+
+let num =5; //ans: 5
+let newNum = num++; //ans: newNum=5; num=6
+newNum = ++num; //ans: newNum=7; num=7
+
+//Declare your name as a string and print its length in JS.
+let fullName = 'Rukhsar Bano';
+fullName.length;
+
+//Declare your first name as a string and print its first character
+let firstName = 'Rukhsar';
+firstName[0];
+
+//Declare your first name as a string and print its last character
+firstName[firstName.length - 1];
+
+
+
