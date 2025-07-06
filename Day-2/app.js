24b6d9e711199e00935335c8a3bc8ed52633790e
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-06 23:57:22 +0530

    Add files via upload

---
+++ Day-2/app.js
@@ -0,0 +1,28 @@
+console.log("Hello World!");
+console.log("Apna College");
+let a = 10;
+let b = 5;
+console.log("sum is :", a + b);
+
+let pencilPrice = 10;
+let erasorPrice = 5;
+console.log("The total price is: ", pencilPrice + erasorPrice, "Rupees.");
+console.log(`The total price is: ${erasorPrice + pencilPrice} Rupees.`);//using template literals
+
+//Arithmetic Operators
+let x = 10;
+let y = 5;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+//Comparison Operator
+let age = 18;
+console.log(age >= 19);
+
+
+
+
