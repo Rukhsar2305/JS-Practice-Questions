b54e3f7cfbe063e0b0d7dacd0d1f4fa2cabcb607
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-07 13:29:31 +0530

    Add files via upload

---
+++ Practice-Qs/Qs1/app.js
@@ -0,0 +1,12 @@
+let color = "yellow";
+
+//traffic light system
+if (color === "red") {
+    console.log("Stop! light color is red.");
+}
+if (color === "yellow") {
+    console.log("Slow down, light color is yellow.");
+}
+if (color === "green") {
+    console.log("Go, light color is green.");
+}
