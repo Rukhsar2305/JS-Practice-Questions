613f15dbfe55efba8fb4b2863992c6fab4a862ac
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-09 19:57:35 +0530

    Add files via upload

---
+++ Assignment-Questions/A-Qs-2/app.js
@@ -0,0 +1,5 @@
+//Take the user's name & age as input using prompts
+let Name = prompt("Please enter your name");
+let age = prompt("Please enter your age");
+
+alert(`${Name} is ${age} years old.`);
