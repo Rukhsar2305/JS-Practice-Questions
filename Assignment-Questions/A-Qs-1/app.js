65e984cb70c3000197b82fa635996e7a1e7867ae
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-09 18:38:18 +0530

    Add files via upload

---
+++ Assignment-Questions/A-Qs-1/app.js
@@ -0,0 +1,9 @@
+//Create a number variable num with some value. Now, print "Good" if the number is divisible by 10 and print "bad" if it is not.
+let num = 30;
+
+if (num % 10 === 0) {
+    console.log("Good variable");
+} else {
+    console.log("Bad");
+}
+
