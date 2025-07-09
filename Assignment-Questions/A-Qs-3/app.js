a0f015c4d4041aecb0812a9bbde7896fa8b4ea12
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-09 21:00:55 +0530

    Add files via upload

---
+++ Assignment-Questions/A-Qs-3/app.js
@@ -0,0 +1,14 @@
+//Write a switch statement to print the months in a quarter
+let quarter = 2;
+
+switch (quarter) {
+    case 1: console.log("January, February, March");
+    break;
+    case 2: console.log("April, May, June");
+    break;
+    case 3: console.log("July, August, September");
+    break;
+    case 4: console.log("October, November, December");
+    break;
+    default: console.log("NOT A QUARTER");
+}
