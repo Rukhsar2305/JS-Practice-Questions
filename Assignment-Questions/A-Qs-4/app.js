b3215f331333920d81e0e183afd185d13848bfdf
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-09 21:08:47 +0530

    Add files via upload

---
+++ Assignment-Questions/A-Qs-4/app.js
@@ -0,0 +1,8 @@
+//A string is a golden string if it starts with the character ‘A’ or ‘a’ and has a total length greater than 5.
+let str = "apples";
+
+if ((str[0]==='a' || start[0] == 'A') && (str.length > 5)) {
+    console.log("golden string");
+} else {
+    console.log("not a golden string");
+}
