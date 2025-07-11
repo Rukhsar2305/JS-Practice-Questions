635df4cd666bb3e2c9a300c46e799dd9d78b0c4f
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-11 20:41:44 +0530

    Add files via upload

---
+++ Practice-Qs/Qs8/app.js
@@ -0,0 +1,10 @@
+let months = ["january", "july", "march", "august"];
+months.splice(0,2,"july", "june");
+console.log(months);
+
+let lang = ['c', 'c++', 'html', 'javascript', 'python', 'java', 'c#'];
+lang.reverse().indexOf('javascript');
+console.log(lang);
+
+
+
