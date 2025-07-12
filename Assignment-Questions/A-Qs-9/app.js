281447b98fa379bc5dbe68b979978065298b4dd2
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-12 12:29:55 +0530

    Add files via upload

---
+++ Assignment-Questions/A-Qs-9/app.js
@@ -0,0 +1,9 @@
+//Write a JavaScript program to test whether the character at the given (character) idx is lower case.
+let str = "apNAcoLlege";
+let idx = 3;
+
+if (str[idx] == str[idx].toLowerCase()){
+    console.log("character is lowercase");
+} else {
+    console.log("character is not lowercase");
+}
