8b963ed0f97d3bab7675543f40d0254d9bf4a344
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-12 12:19:06 +0530

    Add files via upload

---
+++ Assignment-Questions/A-Qs-8/app.js
@@ -0,0 +1,8 @@
+//Write a JavaScript program to check whether the string is blank or not 
+let str = prompt("Please enter a string");
+
+if (str.length === 0){
+    console.log("String is empty");
+} else {
+    console.log("String is not empty");
+}
