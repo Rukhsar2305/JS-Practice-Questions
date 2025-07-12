f652bdd8dbcd3f7f977f4196fef6eeb70ffd6bb0
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-12 12:48:15 +0530

    Add files via upload

---
+++ Assignment-Questions/A-Qs-11/app.js
@@ -0,0 +1,9 @@
+//Write a JavaScript program to check if an element exists in an array or not
+let arr = ["hell0", "a", 23, 64, 99, -6];
+let item = '';
+
+if (arr.indexOf(item) != -1){
+    console.log("element exists in array");
+} else {
+    console.log("element doesn't exists in array");
+}
