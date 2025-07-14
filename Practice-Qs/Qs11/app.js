a8253d8b1f738dc4a85c92834017f8bcd097e3c7
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-14 20:56:28 +0530

    Add files via upload

---
+++ Practice-Qs/Qs11/app.js
@@ -0,0 +1,11 @@
+//Generate a number between 1 to 100
+let num = Math.floor(Math.random()*100) + 1;
+console.log(num);
+
+//Generate a number between 1 to 5
+let number = Math.floor(Math.random()*5) + 1;
+console.log(number);
+
+//Generate a number between 21 to 25
+let no = Math.floor(Math.random()*5) + 20;
+console.log(no);
