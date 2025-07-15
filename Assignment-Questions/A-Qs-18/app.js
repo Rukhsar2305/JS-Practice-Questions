4485e865f9f140da403f300387e18764d1cfb5d0
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-15 12:54:39 +0530

    Add files via upload

---
+++ Assignment-Questions/A-Qs-18/app.js
@@ -0,0 +1,11 @@
+//Create an object Person with their name, age and city. 
+//Edit their city’s original value to change it to “NewYork”. Add a new property country and set it to the UnitedStates
+const person = {
+    name: "Rukhsar",
+    age: 20,
+    city: "Bhilai"
+}
+
+person.city = "New York";
+person.country = "United States";
+console.log(person);
