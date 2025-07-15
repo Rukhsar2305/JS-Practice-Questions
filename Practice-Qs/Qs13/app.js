72b1088bd1de6f6e49012cf866a19f8ae37631cf
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-15 16:07:37 +0530

    Add files via upload

---
+++ Practice-Qs/Qs13/app.js
@@ -0,0 +1,9 @@
+//Create a function that prints a poem
+function poem() {
+    console.log("Twinkle Twinkle little star");
+    console.log("how I wonder what you are");
+    console.log("Up above the world so high");
+    console.log("Like a diamond in the sky");
+}
+
+poem();
