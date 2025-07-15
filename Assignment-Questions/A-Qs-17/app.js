c1ea7924b351ce5956b4e384f57fc2af05101a5d
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-15 12:44:21 +0530

    Add files via upload

---
+++ Assignment-Questions/A-Qs-17/app.js
@@ -0,0 +1,8 @@
+//Create an object representing a car that stores the following properties for the car: name, model, color. Print the car’s name
+const car = {
+    name: "Maruti Suzuki",
+    model: "Maruti Suzuki Dzire",
+    color: "white"
+};
+
+console.log(car.name);
