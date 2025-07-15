e211121a018c54137122b0a823ebd2898083b55c
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-15 19:43:24 +0530

    Add files via upload

---
+++ Practice-Qs/Qs17/app.js
@@ -0,0 +1,10 @@
+//Create a function that returns the sum of numbers from 1 to n.
+function getSum(n){
+    let sum = 0;
+
+    for(let i=1; i<=n; i++){
+        sum+=i;
+    }
+    
+    return sum;
+}
