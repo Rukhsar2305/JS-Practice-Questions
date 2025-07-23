800850650b45f915837154fbcb30ffab36e6cf8e
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-23 23:34:56 +0530

    Add files via upload

---
+++ Assignment-Questions/A-Qs-27/app.js
@@ -0,0 +1,10 @@
+//Square and sum the array elements using the arrow function and then find the average of the array
+let nums = [1, 2, 3, 4, 5];
+
+const square = nums.map( (num) => num*num);
+console.log(square);
+
+let sum = square.reduce((acc, cur) => acc + cur, 0);
+
+let avg = sum / nums.length;
+console.log(avg);
