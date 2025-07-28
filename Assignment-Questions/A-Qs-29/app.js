5c1b7e5f575125397397db00c0abb643208b2d20
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-07-28 23:53:42 +0530

    Add files via upload

---
+++ Assignment-Questions/A-Qs-29/app.js
@@ -0,0 +1,4 @@
+//Create a new array whose elements are in upper case of words present in the original array
+let strings = ["adam", "bob", "catlyn","donald","eve"];
+
+console.log(strings.map((string) => string.toUpperCase()));
